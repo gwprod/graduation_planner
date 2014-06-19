bfd008be390f763675b866750b8a8fd559dce67c
mltbooks <whittomd@cwu.edu>
2014-06-19 10:41:25 -0700

    Changes to fix errors in PAReview

--- graduation_planner_class/templates/graduation-planner-class--full.tpl.php
+++ graduation_planner_class/templates/graduation-planner-class--full.tpl.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * @file
+ * Template for displaying GraduationPlannerClass in view_mode full.
+ */
+
+?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
